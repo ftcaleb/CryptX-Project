809c44b5941109af78cabc34942b3ec915784bad
ftcaleb <caleb19scott@gmail.com>
2025-11-07 15:00:11 +0200

    update

--- CryptX-2/src/Components/Header.jsx
+++ CryptX-2/src/Components/Header.jsx
@@ -1,53 +1,41 @@
 import React from 'react'
-import search from '../Assets/search.svg'
 import { CiBellOn } from "react-icons/ci";
 import { CiCircleQuestion } from "react-icons/ci";
+import { FiSearch, FiChevronDown } from 'react-icons/fi';
 import avatar from '../assets/avatar.png';
 
 export default function Header() {
   return (
-    <div className="flex flex-row items-center justify-around  top-0">
+    <div className="flex flex-row items-center justify-around top-0">
       {/* Search Box */}
-      <div className="flex items-center">
-        <form className="relative">
+        <div className="relative px-3 py-2 flex items-center gap-2">
+          
           <input
             type="text"
             placeholder="Search type of keyword"
-            className="p-2 pl-10 rounded-[5px] border w-68 focus:outline-none"
+            className="Search py-2 px-8  border w-68 focus:outline-none "
           />
-          <img
-            src={search}
-            alt="search icon"
-            className="absolute right-3 top-1/2 transform -translate-y-1/2 w-4 h-4 opacity-70"
-          />
-        </form>
-      </div>
+          <FiSearch className="absolute mr-3 right-3 text-gray-400 outline-none" />
+        </div>
 
-      {/* Icons placeholder */}
-      
-        
-      
-      
 
-      {/* User info */}
-      
-      
-      <div className="flex  items-end">
+      <div className="flex items-center gap-6">
         <CiBellOn size={35} />
-        <CiCircleQuestion size={35} /> 
-        
-        
-        <div className='flex items-end p-1 ml-'>
-        
-        <img src={avatar} alt="User Avatar" className="w-9 h-9 rounded-full mb-1" />
-        <div className="ml-2">
-        <p className="">Laurice</p>
-        <p className="text-sm text-gray-500">Laurice@11</p>
+        <CiCircleQuestion size={35} />
+
+        <div className='flex flex-end items-center gap-3 p-1 ml-45'>
+
+          <img src={avatar} alt="User Avatar" className="w-9 h-9 rounded-full mb-1" />
+
+          <div className="flex flex-col">
+            <p className="">Laurice</p>
+            <p className="text-sm text-gray-500">Laurice@11</p>
+          </div>
+
+          <FiChevronDown />
         </div>
         
-        </div>
-        </div>
-      
+      </div>
     </div>
   )
 }
