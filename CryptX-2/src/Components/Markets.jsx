176aa23765a56f635c0eb1fc16a7251d9cd868a4
ftcaleb <caleb19scott@gmail.com>
2025-11-07 15:06:58 +0200

    update

--- CryptX-2/src/Components/Markets.jsx
+++ CryptX-2/src/Components/Markets.jsx
@@ -4,7 +4,7 @@ import Pattern from "../assets/Pattern.png";
 import Ygraph from "../assets/Ygraph.png"
 import Ggraph from "../assets/Ggraph.png"
 import Bitcoin from '../assets/BitLogo.png';
-import Litecoin from "../assets/litelogo.png"
+import Litecoin from "../assets/Litelogo.png"
 import Cardano from "../assets/CardanoLogo.png"
 
 
